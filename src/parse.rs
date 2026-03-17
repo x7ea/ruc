50970b1c0d945f7635a66b91244b948979bac6cc
れあ命令 <compiler-develop@proton.me>
2026-03-17 18:44:33 +0900

    Update

--- src/parse.rs
+++ src/parse.rs
@@ -29,7 +29,7 @@ impl Expr {
             };
             ($x: expr, $ls: literal, $rs: literal) => {
                 ok!($x.strip_suffix($rs)).and_then(|x| tokenize(x, &$ls))
-                    .and_then(|x| Ok((ok!(x.get(..x.len()-1))?.concat(), ok!(x.last())?)))
+                    .and_then(|x| Ok((ok!(x.get(..x.len()-1))?.concat(), ok!(x.last().clone())?)))
             };
         }
         type Operator = (Box<Expr>, String, Box<Expr>);
