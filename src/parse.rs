141c22c75fa893653f1d118aff7c38f5b17e1b6c
れあ命令 <compiler-develop@proton.me>
2026-03-17 18:21:31 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -28,7 +28,8 @@ impl Expr {
                 $x.strip_prefix($ls).and_then(|x| x.strip_suffix($rs))
             };
             ($x: expr, $ls: literal, $rs: literal) => {
-                $x.strip_suffix($rs).and_then(|x| x.rsplit_once($ls)) 
+                $x.strip_suffix($rs).and_then(|x| tokenize(x, &$ls))
+                    .and_then(|x|Some((x.get(..x.len()-1)?.concat(), x.last()?)))
             };
         }
         type Operator = (Box<Expr>, String, Box<Expr>);
