87d170c9ecddbac48e5c05beab3c5529bd675327
れあ命令 <compiler-develop@proton.me>
2026-03-17 19:14:36 +0900

    d

--- src/parse.rs
+++ src/parse.rs
@@ -30,8 +30,9 @@ impl Expr {
             ($x: expr, $ls: literal, $rs: literal) => {{
                 dbg!(&x);
                 tokenize(x, &$ls).and_then(|x| {
+                      dbg!(&x);
                     let args = ok!(x.last())?.to_string();
-                    let func = ok!(x.get(..x.len()-1))
+                    let func = ok!(x.get(..x.len()-1))?
                         .iter().map(|s| s.to_string()).collect::<String>();
 
                     let args = ok!(args.get(1..args.len()-1))?.to_string();
