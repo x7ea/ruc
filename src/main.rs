14f651c1e43153dd42633bc040f7d08e62259624
Flandre Scarlet <compiler-develop@proton.me>
2026-05-26 23:17:33 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -175,7 +175,7 @@ pub struct Global {
     lib: IndexMap<Name, Type>,
     table: IndexMap<Name, (Args, Object)>,
     def: IndexMap<Name, Define>,
-    pol: IndexSet<Name>,
+    poly: IndexSet<Name>,
 }
 
 #[derive(Default, Debug, Clone)]
