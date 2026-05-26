442673de6b8e2ad1faec1cdf190b191fe3a378bf
Flandre Scarlet <compiler-develop@proton.me>
2026-05-26 22:39:44 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub mod lex;
 pub mod parse;
 pub mod typ;
 
-use indexmap::{IndexMap, IndexSet};
+use indexmap::IndexMap;
 use lex::name::Name;
 use lex::tokenize;
 use ordered_float::OrderedFloat as Float;
