c5263bd7a2d62d2367209567e9c05aa3aa6621cb
Flandre Scarlet <compiler-develop@proton.me>
2026-05-26 21:18:40 +0900

    Update ruca.sh

--- ruca.sh
+++ ruca.sh
@@ -5,8 +5,8 @@ elif [ $1 = "--update" ]; then
     git pull
     cargo install --path .
 else
-    cat $1 | ruca > main.asm & shift
+    cat $1 | ruca > main.asm
     nasm -w-implicit-abs-deprecated -f elf64 -g -F dwarf -o main.o main.asm
     gcc -no-pie -z noexecstack -O3 -rdynamic -o main main.o
-    ./main "$@"
+    ./main
 fi
