b8fb59635d862b76099820c174ef52d834777182
Flandre Scarlet <compiler-develop@proton.me>
2026-05-26 23:18:39 +0900

    Update

--- src/emit.rs
+++ src/emit.rs
@@ -7,7 +7,7 @@ impl Define {
         let Define::Function(Generics(name, _), args, body) = self else {
             return Ok(String::new());
         };
-        if ctx.global.poly.contains(name) {
+        if ctx.global.meta.contains(name) {
             return Ok(String::new());
         }
         ctx.local = ctx.table.get(name).unwrap().clone();
--- src/main.rs
+++ src/main.rs
@@ -175,7 +175,7 @@ pub struct Global {
     lib: IndexMap<Name, Type>,
     table: IndexMap<Name, (Args, Object)>,
     def: IndexMap<Name, Define>,
-    poly: IndexSet<Name>,
+    meta: IndexSet<Name>,
 }
 
 #[derive(Default, Debug, Clone)]
