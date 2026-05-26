35a75c7e0f6547c90966aad2ef4dee9c6994cdd6
Flandre Scarlet <compiler-develop@proton.me>
2026-05-26 19:48:29 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -4,7 +4,7 @@ pub const ABI: [&str; 6] = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"];
 
 impl Define {
     pub fn emit(&self, ctx: &mut Context) -> Result<String, String> {
-        let Define::Function(Generics(name), args, body) = self else {
+        let Define::Function(Generics(name, _), args, body) = self else {
             return Ok(String::new());
         };
         ctx.local = ctx.table.get(name).unwrap().clone();
