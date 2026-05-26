46a243a4f370e5c23b96aa679ad847327de12d22
Flandre Scarlet <compiler-develop@proton.me>
2026-05-26 22:09:29 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -324,7 +324,7 @@ impl Expr {
             }
             Expr::Member(obj, key) => {
                 let typ = obj.infer(ctx)?;
-                let Type::Class(class @ Generics(name, _)) = &typ else {
+                let Type::Class(Generics(name, _)) = &typ else {
                     return Err(format!("not class: {typ}"));
                 };
                 let Some((_, class)) = ctx.global.table.get(name) else {
