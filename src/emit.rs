4e435f4b8a5c0e8589d0b7a52e9e72fef16947ad
Flandre Scarlet <compiler-develop@proton.me>
2026-05-28 11:59:41 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -195,7 +195,7 @@ impl Expr {
                 }
                 _ => expr!(self).emit(ctx),
             },
-            Expr::Array(_, len) => {
+            Expr::Alloc(_, len) => {
                 let setlen = &format!("\tmov qword [rax], {len}\n");
                 Ok(expr!(self).emit(ctx)? + setlen)
             }
