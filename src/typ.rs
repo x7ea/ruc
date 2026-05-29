bc6b184bca5a4ff7c816692da1db13a9f027314a
Flandre Scarlet <compiler-develop@proton.me>
2026-05-29 23:58:17 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -230,11 +230,10 @@ impl Expr {
                 typing!(ret.clone())
             }
             Expr::Call(callee, args) => {
-                if let Some(obj) = args.first() {
-                    if let Type::Class(Generics(name, _)) = obj.infer(ctx)? {
+                if let Some(obj) = args.first()
+                    && let Type::Class(Generics(name, _)) = obj.infer(ctx)? {
                         ctx.local.class = Some(name);
                     }
-                }
                 let typ = callee.infer(ctx)?;
                 if let Type::Function(_, ret, params) = typ {
                     let Some(params) = params else {
