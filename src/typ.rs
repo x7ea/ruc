bb8867ba001f50ec14fb2c4102d8c5638c9ab754
Flandre Scarlet <compiler-develop@proton.me>
2026-05-29 21:20:42 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -226,6 +226,9 @@ impl Expr {
                     }
                     for (param, arg) in params.iter().zip(args) {
                         let arg = arg.infer(ctx)?.solve(ctx);
+                        if param == &Type::None {
+                            continue;
+                        }
                         if param.solve(ctx) != arg {
                             return Err(format!("arguments: {param} != {arg}"));
                         }
