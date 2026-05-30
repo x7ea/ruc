74a2dad711765aee1b4bd0d3856b20981037d224
Flandre Scarlet <compiler-develop@proton.me>
2026-05-30 15:26:07 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -36,7 +36,7 @@ impl Define {
                 let body = if let Some(typ) = body.trim().strip_prefix("->") {
                     if let Ok((typ, expr)) = once!(func, SPACE) {
                         (
-                            Some(Expr::Block(vec![Expr::parse(&body)?])),
+                            Some(Expr::Block(vec![Expr::parse(&expr)?])),
                             Some(Type::parse(&typ)?),
                         )
                     } else {
