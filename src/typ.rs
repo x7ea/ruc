a3ac1ddf9edc685cd3cf9ed58904f5f2ea547d2b
Flandre Scarlet <compiler-develop@proton.me>
2026-06-04 12:15:15 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -468,7 +468,7 @@ impl Expr {
                         expand!(Expr::If(
                             Box::new(Expr::Check(Box::new(self.clone()))),
                             Box::new(Expr::Read(offset, typ.clone(), obj.clone())),
-                            Some(Box::new(Expr::Null(Some(typ.clone()))))
+                            Some(Box::new(Expr::Null(typ.clone())))
                         ));
                     }
                 }
