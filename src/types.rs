be9e9c6d83864e75e7de4f5f93f4d55e9b163c9a
Flandre Scarlet <compiler-develop@proton.me>
2026-06-04 12:37:05 +0900

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -144,7 +144,7 @@ impl Expr {
                     fmt += match typ {
                         Type::Int => "%ld",
                         Type::Float => "%g",
-                        Type::String => "%s",
+                        Type::Str => "%s",
                         _ => return Err(format!("can't print: {typ}")),
                     }
                 }
@@ -156,7 +156,7 @@ impl Expr {
                     Box::new(Expr::Variable(Generics(Name::new(name)?, vec![]))),
                     [vec![Expr::String(fmt)], vals.to_vec()].concat(),
                 ));
-                typing!(if is_output { Type::None } else { Type::String })
+                typing!(if is_output { Type::None } else { Type::Str })
             }
             Expr::If(cond, then, els) => {
                 if let Expr::Let(bind, check) = *cond {
@@ -561,7 +561,7 @@ impl Expr {
             }
             Expr::Integer(_) => typing!(Type::Int),
             Expr::Float(_) => typing!(Type::Float),
-            Expr::String(_) => typing!(Type::String),
+            Expr::String(_) => typing!(Type::Str),
             Expr::Bool(_) => typing!(Type::Bool),
             Expr::Add(lhs, rhs)
             | Expr::Sub(lhs, rhs)
