1d93b8e7b6451adb2597bca6acee7b38e7dea5a0
Flandre Scarlet <compiler-develop@proton.me>
2026-06-04 12:57:22 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -66,8 +66,8 @@ impl Expr {
                         "{lhs}{push}{rhs}\tmovsd xmm1, xmm0\n{pop}\t{op}sd xmm0, xmm1\n",
                         lhs = $lhs.emit(ctx)?, rhs = $rhs.emit(ctx)?,
                         push = "\tsub rsp, 8\n\tmovsd [rsp], xmm0\n",
-                        pop = "\tmovsd xmm0, [rsp]\n\tadd rsp, 8\n"
-                        op = $asm.replace("imul", "mul"),
+                        pop = "\tmovsd xmm0, [rsp]\n\tadd rsp, 8\n",
+                        op = $asm.replace("imul", "mul")
                     ),
                     _ => panic!(),
                 }
