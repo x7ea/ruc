206d3ed1868e8c29372cb6056e4a62ddce84c3c5
Flandre Scarlet <compiler-develop@proton.me>
2026-06-04 12:41:29 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -57,7 +57,7 @@ pub mod name {
             fn mangle(typ: &Type) -> String {
                 match typ {
                     Type::Int => "I".to_string(),
-                    Type::Str => "S".to_string(),
+                    Type::String => "S".to_string(),
                     Type::Bool => "B".to_string(),
                     Type::None => "N".to_string(),
                     Type::Float => "F".to_string(),
