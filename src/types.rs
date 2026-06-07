460531a697215ff5e5ce674ae6448c7baf8315d5
Flandre Scarlet <compiler-develop@proton.me>
2026-06-07 22:00:52 +0900

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -284,7 +284,7 @@ impl Expr {
                     };
                     let (pl, al) = (params.len(), args.len());
                     if pl != al {
-                        return Err(format!("length: {pl} != {al}",));
+                        return Err(format!("length: {pl} != {al}"));
                     }
                     for (param, arg) in params.iter().zip(args) {
                         let arg = arg.infer(ctx)?.solve(ctx);
