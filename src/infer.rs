25e538dd04a4dc84ff5fa176f158b92513de5804
Flandre Scarlet <compiler-develop@proton.me>
2026-06-08 18:36:05 +0900

    Update infer.rs

--- src/infer.rs
+++ src/infer.rs
@@ -53,6 +53,7 @@ impl Expr {
                 class.clone()
             }};
         }
+        
         match self.clone() {
             Expr::Print(is_output, vals) => {
                 let mut fmt = String::new();
