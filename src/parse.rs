f34667d64ec04055e5c02d3c788ed512bfbbc878
Flandre Scarlet <compiler-develop@proton.me>
2026-06-08 12:04:01 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -231,10 +231,7 @@ impl Type {
             "()" => Ok(Type::Void),
             x => {
                 if let Ok((ret, args)) = surround!(x, "(", ")") {
-                    Ok(Type::Function(
-                        Vec::new(), Box::new(Type::parse(&ret)?),
-                        Some(serial!(&args, Type::parse)),
-                    ))
+                    Ok(Type::Function(Vec::new(), Box::new(Type::parse(&ret)?), Some(serial!(&args, Type::parse))))
                 } else if let Some(typ) = surround!("[", x, "]") {
                     Ok(Type::Array(Box::new(Type::parse(typ)?)))
                 } else {
