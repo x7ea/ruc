5e8ac8fb66ee1140185c266c2934c03072564dd1
Flandre Scarlet <compiler-develop@proton.me>
2026-06-08 11:28:38 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -7,11 +7,9 @@ impl Define {
         let Define::Function(Generics(name, params), args, (Some(body), _)) = self else {
             return Ok(String::new());
         };
-        if !params.is_empty() {
-            return Ok(String::new());
-        }
+        if !params.is_empty() { return Ok(String::new()); }
+        
         ctx.local = ctx.table.get(name).unwrap().clone();
-
         let (mut ptr, mut alloc) = (8, String::new());
         let (mut idx, mut xmm) = (0, 0);
         for (var, (_, typ)) in args.iter().enumerate() {
