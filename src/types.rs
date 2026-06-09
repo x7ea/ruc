bf9ecf3fe62e03f8c67ca4d910dc459dea853931
Flandre Scarlet <compiler-develop@proton.me>
2026-06-09 17:25:06 +0900

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -124,6 +124,9 @@ impl Expr {
                         _ => return Err(format!("can't print: {typ}")),
                     }
                 }
+                if is_output {
+                    fmt += "\\n"
+                };
                 expand!(Expr::Call(
                     Box::new(Expr::Variable(Generics(
                         Name::new(if is_output {
@@ -133,11 +136,7 @@ impl Expr {
                         })?,
                         vec![]
                     ))),
-                    [
-                        vec![Expr::String(if is_output { fmt + "\\n" } else { fmt })],
-                        vals.to_vec()
-                    ]
-                    .concat(),
+                    [vec![Expr::String(fmt)], vals.to_vec()].concat(),
                 ));
                 typing!(if is_output { Type::Void } else { Type::String })
             }
