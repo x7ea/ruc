d2b69e310a7b7ab3b138533acab75f237762b0a0
Flandre Scarlet <compiler-develop@proton.me>
2026-06-11 16:47:12 +0900

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -173,65 +173,6 @@ impl Expr {
                 }
                 body.infer(ctx)
             }
-            Expr::Match(val, pats) => {
-                let mut expr = Expr::Null(Type::Void);
-                for (key, bind, ret) in pats {
-                    let acc = Box::new(Expr::Member(val.clone(), key.clone()));
-                    expr = Expr::If(
-                        if let Some(bind) = bind {
-                            Box::new(Expr::Let(Box::new(bind.clone()), acc))
-                        } else {
-                            Box::new(Expr::Check(acc))
-                        },
-                        Box::new(ret.clone()),
-                        Some(Box::new(expr)),
-                    )
-                }
-                typing!(expands!(expr))
-            }
-            Expr::Enum(typ, key, val) => {
-                let temp = Box::new(temp!(typ.clone()));
-                typing!(expands!(Expr::Block(vec![
-                    Expr::Let(temp.clone(), Box::new(Expr::New(typ.clone()))),
-                    Expr::Let(
-                        Box::new(Expr::Member(temp.clone(), key.clone())),
-                        val.clone(),
-                    ),
-                    *temp
-                ])))
-            }
-            Expr::For(cnt, arr, body) => {
-                let temp = Box::new(temp!(Type::Integer));
-                let read = Box::new(Expr::Index(arr.clone(), temp.clone()));
-                let inc = Box::new(Expr::Add(temp.clone(), Box::new(Expr::Integer(1))));
-                let body = [Expr::Let(cnt, read), *body, Expr::Let(temp.clone(), inc)];
-                typing!(expands!(Expr::Block(vec![
-                    Expr::Let(temp.clone(), Box::new(Expr::Integer(0))),
-                    Expr::While(
-                        Box::new(Expr::Lt(temp.clone(), len!(arr))),
-                        Box::new(Expr::Block(body.to_vec()))
-                    ),
-                ])))
-            }
-            Expr::Sequence(array) => {
-                let typ = array[0].infer(ctx)?;
-                let temp = temp!(typ.clone());
-                let mut expr = vec![Expr::Let(
-                    Box::new(temp.clone()),
-                    Box::new(Expr::Init(typ, array.len())),
-                )];
-                for (idx, val) in array.iter().enumerate() {
-                    expr.push(Expr::Let(
-                        Box::new(Expr::Index(
-                            Box::new(temp.clone()),
-                            Box::new(Expr::Integer(idx as i64)),
-                        )),
-                        Box::new(val.clone()),
-                    ));
-                }
-                expr.push(temp);
-                typing!(expands!(Expr::Block(expr)))
-            }
             Expr::Block(lines) => {
                 let mut ret = Type::Void;
                 let parent = ctx.local.scope.clone();
@@ -444,18 +385,6 @@ impl Expr {
                 addr.infer(ctx)?;
                 typing!(val.infer(ctx)?)
             }
-            Expr::Clone(expr) => {
-                let typ = expr.infer(ctx)?;
-                let dest = Box::new(temp!(typ));
-                typing!(expands!(Expr::Block(vec![
-                    Expr::Let(dest.clone(), Box::new(Expr::New(typ.clone()))),
-                    Expr::Call(
-                        Box::new(Expr::Variable(Generics(Name::new("memcpy")?, vec![]))),
-                        vec![*dest.clone(), *expr, Expr::Integer(typ.size(ctx)? as i64)]
-                    ),
-                    *dest.clone()
-                ])))
-            }
             Expr::Mod(lhs, rhs) => {
                 expand!(Expr::Div(lhs.clone(), rhs.clone()));
                 op!(Type::Integer, lhs, rhs)
@@ -487,6 +416,77 @@ impl Expr {
             | Expr::LtEq(lhs, rhs) => op!(Type::Integer, lhs, rhs, Type::Boolean),
             Expr::And(lhs, rhs) | Expr::Or(lhs, rhs) | Expr::Xor(lhs, rhs) => {
                 op!(Type::Boolean, lhs, rhs)
+            }
+            Expr::Match(val, pats) => {
+                let mut expr = Expr::Null(Type::Void);
+                for (key, bind, ret) in pats {
+                    let acc = Box::new(Expr::Member(val.clone(), key.clone()));
+                    expr = Expr::If(
+                        if let Some(bind) = bind {
+                            Box::new(Expr::Let(Box::new(bind.clone()), acc))
+                        } else {
+                            Box::new(Expr::Check(acc))
+                        },
+                        Box::new(ret.clone()),
+                        Some(Box::new(expr)),
+                    )
+                }
+                typing!(expands!(expr))
+            }
+            Expr::Enum(typ, key, val) => {
+                let temp = Box::new(temp!(typ.clone()));
+                typing!(expands!(Expr::Block(vec![
+                    Expr::Let(temp.clone(), Box::new(Expr::New(typ.clone()))),
+                    Expr::Let(
+                        Box::new(Expr::Member(temp.clone(), key.clone())),
+                        val.clone(),
+                    ),
+                    *temp
+                ])))
+            }
+            Expr::For(cnt, arr, body) => {
+                let temp = Box::new(temp!(Type::Integer));
+                let read = Box::new(Expr::Index(arr.clone(), temp.clone()));
+                let inc = Box::new(Expr::Add(temp.clone(), Box::new(Expr::Integer(1))));
+                let body = [Expr::Let(cnt, read), *body, Expr::Let(temp.clone(), inc)];
+                typing!(expands!(Expr::Block(vec![
+                    Expr::Let(temp.clone(), Box::new(Expr::Integer(0))),
+                    Expr::While(
+                        Box::new(Expr::Lt(temp.clone(), len!(arr))),
+                        Box::new(Expr::Block(body.to_vec()))
+                    ),
+                ])))
+            }
+            Expr::Sequence(array) => {
+                let typ = array[0].infer(ctx)?;
+                let temp = temp!(typ.clone());
+                let mut expr = vec![Expr::Let(
+                    Box::new(temp.clone()),
+                    Box::new(Expr::Init(typ, array.len())),
+                )];
+                for (idx, val) in array.iter().enumerate() {
+                    expr.push(Expr::Let(
+                        Box::new(Expr::Index(
+                            Box::new(temp.clone()),
+                            Box::new(Expr::Integer(idx as i64)),
+                        )),
+                        Box::new(val.clone()),
+                    ));
+                }
+                expr.push(temp);
+                typing!(expands!(Expr::Block(expr)))
+            }
+            Expr::Clone(expr) => {
+                let typ = expr.infer(ctx)?;
+                let dest = Box::new(temp!(typ));
+                typing!(expands!(Expr::Block(vec![
+                    Expr::Let(dest.clone(), Box::new(Expr::New(typ.clone()))),
+                    Expr::Call(
+                        Box::new(Expr::Variable(Generics(Name::new("memcpy")?, vec![]))),
+                        vec![*dest.clone(), *expr, Expr::Integer(typ.size(ctx)? as i64)]
+                    ),
+                    *dest.clone()
+                ])))
             }
         }
     }
