e6290610d47a1e60675318d6e8991e7ecd229d5b
Flandre Scarlet <compiler-develop@proton.me>
2026-06-11 17:31:02 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -547,30 +547,3 @@ macro_rules! ok {
         }
     };
 }
-
-#[macro_export]
-macro_rules! new {
-    ($layout: expr) => {
-        Expr::Call(
-            Box::new(Expr::Variable(Generics(Name::new("calloc")?, vec![]))),
-            vec![Expr::Integer($layout as i64), Expr::Integer(8)],
-        )
-    };
-}
-
-#[macro_export]
-macro_rules! len {
-    ($arr: expr) => {
-        Box::new(Expr::Member($arr.clone(), Name::new("len")?))
-    };
-}
-
-#[macro_export]
-macro_rules! array {
-    ($arr: expr, $idx: expr) => {
-        Box::new(Expr::Add(
-            Box::new(Expr::Mod($idx.clone(), len!($arr))),
-            Box::new(Expr::Integer(1)),
-        ))
-    };
-}
