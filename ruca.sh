1ed19f6183a5176d3d12c5c229ef5e17dd402abd
Flandre Scarlet <compiler-develop@proton.me>
2026-06-14 11:17:29 +0900

    Update ruca.sh

--- ruca.sh
+++ ruca.sh
@@ -7,6 +7,15 @@ elif [ $1 = "--test" ]; then
     for file in $(find ./app -type f); do
         ./ruca.sh $file
     done
+elif [ $1 = "--demo" ]; then
+    for file in $(find ./app -type f); do
+        clear
+        echo "Example: $file"
+        cat $file
+        read
+        ./ruca.sh $file
+        read
+    done
 else
     cat $1 | ruca > main.asm
     nasm -w-implicit-abs-deprecated -f elf64 -g -F dwarf -o main.o main.asm
