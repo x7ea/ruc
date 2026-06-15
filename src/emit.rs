dd8b1c555879816b0621b0112068d15c133cd2be
Flandre Scarlet <compiler-develop@proton.me>
2026-06-15 21:11:16 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -135,8 +135,8 @@ impl Expr {
         }
         match self {
             Expr::If(cond, then, els) => {
-                let with_let = ctx.local.expand.get(&self.clone());
-                if let Some(expr) = with_let.cloned() {
+                let if_let = ctx.local.expand.get(&self.clone());
+                if let Some(expr) = if_let.cloned() {
                     return expr.emit(ctx);
                 }
                 let id = ctx.label();
@@ -153,8 +153,8 @@ impl Expr {
                 }
             }
             Expr::While(cond, body) => {
-                let with_let = ctx.local.expand.get(&self.clone());
-                if let Some(expr) = with_let.cloned() {
+                let while_let = ctx.local.expand.get(&self.clone());
+                if let Some(expr) = while_let.cloned() {
                     return expr.emit(ctx);
                 }
                 let id = ctx.label();
