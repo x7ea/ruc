55262dc35a279032d69f6bd08e1624c29fd67b9f
Flandre Scarlet <compiler-develop@proton.me>
2026-06-16 17:21:39 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -476,7 +476,7 @@ impl Expr {
             Expr::Add(lhs, rhs)
             | Expr::Sub(lhs, rhs)
             | Expr::Mul(lhs, rhs)
-            | Expr::Div(lhs, rhs) => op!((Type::Integer | Type::Float), lhs, rhs),
+            | Expr::Div(lhs, rhs) => op!(Type::Integer | Type::Float, lhs, rhs),
             Expr::Eq(lhs, rhs)
             | Expr::Ne(lhs, rhs)
             | Expr::Gt(lhs, rhs)
