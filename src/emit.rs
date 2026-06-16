9b4c2f179036f1776b2045c0faf291643860ae9e
Flandre Scarlet <compiler-develop@proton.me>
2026-06-16 16:33:52 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -311,7 +311,7 @@ impl Expr {
             Expr::Sub(lhs, rhs) => Ok(op!("sub", lhs, rhs)),
             Expr::Mul(lhs, rhs) => Ok(op!("imul", lhs, rhs)),
             Expr::Eql(lhs, rhs) => Ok(cmp!("e", lhs, rhs)),
-            Expr::NotEq(lhs, rhs) => Ok(cmp!("ne", lhs, rhs)),
+            Expr::Neq(lhs, rhs) => Ok(cmp!("ne", lhs, rhs)),
             Expr::Gt(lhs, rhs) => Ok(cmp!("g", lhs, rhs)),
             Expr::Lt(lhs, rhs) => Ok(cmp!("l", lhs, rhs)),
             Expr::GtEq(lhs, rhs) => Ok(cmp!("ge", lhs, rhs)),
