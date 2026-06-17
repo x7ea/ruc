de7aaa99378dc735166cbce636af351c7fbced2d
Flandre Scarlet <compiler-develop@proton.me>
2026-06-17 18:43:25 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -78,10 +78,8 @@ impl Expr {
             let tokens: Vec<String> = lexer(src, SPACE)?;
             if tokens.len() >= 3 {
                 let pos: usize = tokens.len() - 2;
-                let opr = tokens[pos].to_string();
-                let lhs = tokens[..pos].join(SPACE);
-                let rhs = tokens[pos + 1].to_string();
-                Ok((lhs, opr, rhs))
+                let (lhs, rhs) = (tokens[..pos].join(SPACE), tokens[pos + 1].to_string());
+                Ok((lhs, tokens[pos].to_string(), rhs))
             } else {
                 Err(String::new())
             }
