a361f29279f5227f7dc06fe8294918bf1eab0b6e
Flandre Scarlet <compiler-develop@proton.me>
2026-06-17 22:55:54 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -501,8 +501,7 @@ impl Type {
                 let parent = ctx.global.alias.clone();
                 ctx.global.alias = alias.clone();
 
-                let mangle = func.generics();
-                let mut unify = ctx.global.def[&name].clone();
+                let (mangle, mut unify) = (func.generics(), ctx.global.def[&name].clone());
                 if let Function((_, params), (_, ret)) | Declare((_, params), ret) = unify.clone()
                     && let Type::Function(Lambda(_, _, Some(args))) = typ.clone()
                 {
