42dd12ac80d12518757631ba29e7aa08780c5993
Flandre Scarlet <compiler-develop@proton.me>
2026-06-17 18:08:10 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -309,10 +309,10 @@ impl Expr {
                     rhs.emit(ctx)?,
                 ))
             }
-            Expr::Mod(_, _) => {
-                let modulo = "\tadd rdx, rsi\n\tmov rax, rdx\n\tcqo\n\tidiv rsi\n\tmov rax, rdx\n";
-                Ok(expr!(self).emit(ctx)? + modulo)
-            }
+            Expr::Mod(_, _) => Ok(format!(
+                "{}\tadd rdx, rsi\n\tmov rax, rdx\n\tcqo\n\tidiv rsi\n\tmov rax, rdx\n",
+                expr!(self).emit(ctx)?
+            )),
             Expr::Add(lhs, rhs) => Ok(op!("add", lhs, rhs)),
             Expr::Sub(lhs, rhs) => Ok(op!("sub", lhs, rhs)),
             Expr::Mul(lhs, rhs) => Ok(op!("mul", lhs, rhs)),
