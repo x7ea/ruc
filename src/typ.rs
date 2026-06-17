e59c9e96998eb6d55f6b4ba3706501c9e892a5ac
Flandre Scarlet <compiler-develop@proton.me>
2026-06-17 19:53:50 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -186,7 +186,7 @@ impl Expr {
                 }
                 for (name, val) in &ctx.local.scope {
                     if let Some(typ) = ctx.local.var.get(name)
-                        && typ != val.solve(ctx)
+                        && typ.solve(ctx) != val.solve(ctx)
                     {
                         return Err(format!("duplicated {name}: {typ} != {val}"));
                     }
