f5cd224ecb0130ff5cbd6e6205e99016597e0856
Flandre Scarlet <compiler-develop@proton.me>
2026-06-17 14:08:53 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -202,12 +202,12 @@ impl Expr {
                     Type::Function(Lambda(_, ret, Some(params))) => {
                         let (pl, al) = (params.len(), args.len());
                         if pl != al {
-                            return Err(format!("arguments length: {pl} != {al}"));
+                            return Err(format!("f(x, ..) {pl} != {al}"));
                         }
                         for (param, arg) in params.iter().zip(args) {
                             let arg = arg.infer(ctx)?.solve(ctx);
                             if arg != param.solve(ctx) {
-                                return Err(format!("argument types: {param} != {arg}"));
+                                return Err(format!("f(x: T) {param} != {arg}"));
                             }
                         }
                         typing!(*ret.clone())
