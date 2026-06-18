d6b88b612895fe8acd18220f0ecf15b057307144
Flandre Scarlet <compiler-develop@proton.me>
2026-06-18 10:34:29 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -156,7 +156,7 @@ impl Expr {
                 };
             }
             op!("+"  => Add, "-"  => Sub, "*" => Mul, "/" => Div, "%"  => Mod,
-                "==" => Eq,  "!=" => Ne,  ">" => Gt,  "<" => Lt,  ">=" => Ge, "<=" =>Le,
+                "==" => Eq,  "!=" => Ne,  ">" => Gt,  "<" => Lt,  ">=" => Ge, "<=" => Le,
                 "&"  => And, "|"  => Or,  "^" => Xor,)
         } else if src == "()" {
             Ok(Expr::Null(Type::Void))
