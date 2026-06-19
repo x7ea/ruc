31a65879120e9c1816e468a8654f1515c194ac77
Flandre Scarlet <compiler-develop@proton.me>
2026-06-19 19:02:16 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -8,6 +8,7 @@ impl Define {
                     Define::Function((Generics(func, _), _), _) => func,
                     Define::Class(Generics(class, _), _) => class,
                     Define::Declare((Generics(lib, _), _), _) => lib,
+                    Define::Symbol(sym) => sym,
                 }
                 .clone()
             };
