fac0eaa78041f462d7c66c8471344f39f719cec1
Flandre Scarlet <compiler-develop@proton.me>
2026-06-19 21:03:37 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -142,7 +142,11 @@ impl Expr {
                     }
                 };
             }
-            op!("+" => Add, "-" => Sub, "*" => Mul, "/" => Div, "%" => Mod, "==" => Eq, "!=" => Ne, ">" => Gt, "<" => Lt, ">=" => Ge, "<=" => Le, "&" => And, "|" => Or, "^" => Xor,)
+            op!(
+                "+"  => Add, "-"  => Sub, "*" => Mul, "/" => Div, "%" => Mod,
+                "==" => Eq , "!=" => Ne,  ">" => Gt,  "<" => Lt,  ">=" => Ge, "<=" => Le,
+                "&"  => And, "|"  => Or,  "^" => Xor,
+            )
         } else if let Some(text) = surround!("\"", src, "\"") {
             Ok(Expr::String(text.to_owned()))
         } else if src == "()" {
