bf6467a037bc1efbccdd313084e2165b9847615d
Flandre Scarlet <compiler-develop@proton.me>
2026-06-22 14:34:07 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,7 +16,7 @@ pub fn lexer(src: &str, del: &str) -> Result<Vec<String>, String> {
             continue;
         }
         if let Some(op) = src.get(idx..idx + 3)
-            && [" < ", " > ", " <=", " >="].contains(&op)
+            && [" < ", " > ", "<= ", " >="].contains(&op)
         {
             if del == SPACE && level == 0 {
                 tokens.append(&mut vec![current.clone(), op.trim().to_string()]);
