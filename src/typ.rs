bae0d8981755c2d83042a2492f2fcc92a433fedc
Flandre Scarlet <compiler-develop@proton.me>
2026-06-24 15:22:50 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -85,7 +85,6 @@ impl Expr {
             ($typ: pat, $lhs: expr, $rhs: expr) => {{
                 match ($lhs.infer(ctx)?, $rhs.infer(ctx)?) {
                     ($typ, ret @ $typ) => typing!(ret),
-                    (ret @ Type::Any(_), Type::Any(_)) => typing!(ret),
                     (Type::Any(i), ret @ $typ) | (ret @ $typ, Type::Any(i)) => solve!(ret, i),
                     (lhs, rhs) if lhs != rhs => Err(format!("operator term: {lhs} != {rhs}")),
                     (typ, _) => typing!(expands!(Expr::Call(
