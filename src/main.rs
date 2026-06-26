0efa173c42a3303197386fb5f5d18c5a6c73ac60
Flandre Scarlet <compiler-develop@proton.me>
2026-06-26 20:42:39 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,7 @@ fn main() {
             error!(stdin().read_to_string(&mut buffer));
             buffer.trim().to_owned()
         };
-        let ast = error!(Define::parse(&code));
-        let output = error!(Define::compile(&ast));
+        let output = error!(Define::compile(&error!(Define::parse(&code))));
         error!(stdout().write_all(output.as_bytes()));
     };
     let thread = Builder::new().stack_size(8 * 1024 * 1024);
