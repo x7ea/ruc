16fbbcefa358348020c171e95ad1578320d22070
Flandre Scarlet <compiler-develop@proton.me>
2026-07-02 13:13:10 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -77,7 +77,7 @@ impl Expr {
                     ($typ, ret @ $typ) => typing!(ret.clone()),
                     (lhs, rhs) if lhs != rhs => Err(format!("operator term: {lhs} != {rhs}")),
                     (typ, _) => typing!(expands!(Expr::Call(
-                        Box::new(var!(self.as_ref(), typ.without_generics())),
+                        Box::new(var!(self.as_ref(), typ)),
                         vec![*$lhs, *$rhs],
                     ))),
                 }
