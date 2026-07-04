6debf797c38b8fe87ab4de45bd22a0cd251e4811
Flandre Scarlet <compiler-develop@proton.me>
2026-07-04 14:28:59 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -105,7 +105,7 @@ pub mod name {
             Name(format!("{self}.{typ:?}"))
         }
     }
-    impl fmt::Display for Name {
+    impl Display for Name {
         fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
             write!(f, "{}", self.0)
         }
