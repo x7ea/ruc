f8c87d1361a8dac566abe0e3907900a7de534db2
Flandre Scarlet <compiler-develop@proton.me>
2026-07-14 15:48:44 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -240,8 +240,10 @@ impl Expr {
                     ctx.local.class = None;
                 }
                 if let Some(typ) = ctx.global.lib.get(&name).cloned() {
-                    let var = Generic(name.clone(), map!(args, |x| x.solve(ctx)));
-                    ctx.local.expand.insert(self.clone(), Expr::Variable(var));
+                    let var = Expr::Variable(Generic(name.clone(), map!(args, |x| x.solve(ctx))));
+                    if self != var {
+                        ctx.local.expand.insert(self.clone(), var);
+                    }
                     typing!(typ.mono(ctx, Generic(name, args))?)
                 } else if let Some(typ) = ctx.local.scope.get(&name) {
                     typing!(typ.clone().solve(ctx))
