82cce9899960ee84038c4c324e1fc1f39e19e54e
Flandre Scarlet <compiler-develop@proton.me>
2026-07-15 14:34:49 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -235,9 +235,9 @@ impl Expr {
                     }
                 ))
             }
-            Expr::Integer(val) if *val == 0 => Ok("\txor rax, rax\n".to_string()),
+            Expr::Integer(val) if *val == 0 => Ok(String::from("\txor rax, rax\n")),
             Expr::Integer(val) => Ok(format!("\tmov rax, {val}\n")),
-            Expr::Float(val) if *val == Float(0.0) => Ok("\tpxor xmm0, xmm0\n".to_string()),
+            Expr::Float(val) if *val == Float(0.0) => Ok(String::from("\tpxor xmm0, xmm0\n")),
             Expr::Float(val) => {
                 let name = format!("float.{}", label!());
                 ctx.global.data += &format!("\t{name} dq {val:?}\n");
