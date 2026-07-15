03da5e9f048657dec4fe68907eb76e0106e25afc
Flandre Scarlet <compiler-develop@proton.me>
2026-07-15 15:18:01 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -63,6 +63,7 @@ impl Define {
         }
         let body = body.emit(ctx)?;
         ctx.table.insert(name.clone(), ctx.local.clone());
+
         let var = ctx.local.var.len() * 8;
         let pro = format!(
             "\tpush rbp\n\tmov rbp, rsp\n\tsub rsp, {}\n",
