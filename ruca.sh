9156a7e1abaf90368e481bfb10d48fdb36b680db
Flandre Scarlet <compiler-develop@proton.me>
2026-07-20 14:17:48 +0900

    Update ruca.sh

--- ruca.sh
+++ ruca.sh
@@ -1,15 +1,4 @@
-if  [ "$#" -lt 1 ]; then
-    cat ./README.md
-elif [ $1 = "--update" ]; then
-    git stash & git pull
-    cargo install --path .
-elif [ $1 = "--test" ]; then
-    for file in $(find ./app -type f); do
-        ./ruca.sh $file
-    done
-else
-    cat $1 | ruca > main.asm
-    nasm -w-implicit-abs-deprecated -f elf64 -g -F dwarf -o main.o main.asm
-    gcc main.o $(pkg-config --cflags --libs gtk+-3.0) -no-pie -z noexecstack -O3 -rdynamic -o main
-    ./main
-fi
+cat $1 | ruca > main.asm
+nasm -w-implicit-abs-deprecated -f elf64 -g -F dwarf -o main.o main.asm
+gcc main.o $(pkg-config --cflags --libs gtk+-3.0) -no-pie -z noexecstack -O3 -rdynamic -o main
+./main
