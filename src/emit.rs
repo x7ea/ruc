51941e3f66e0e465abc05633a2f5904d6289bddf
Flandre Scarlet <compiler-develop@proton.me>
2026-07-21 17:53:02 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -339,5 +339,5 @@ macro_rules! var {
             $typ.generic_args(),
         ))
     }};
-    ($name: expr, $args: expr, $typ: expr) => {{ Expr::Variable(Generic(Name::new($name)?, $args + &$typ.generic_args())) }};
+    ($name: expr, $args: expr, $typ: expr) => {{ Expr::Variable(Generic(Name::new($name)?, [args, &$typ.generic_args())].clone()) }};
 }
