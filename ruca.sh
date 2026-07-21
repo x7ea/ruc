0a715c27cecf932d8810ba682141b36a6e636247
Flandre Scarlet <compiler-develop@proton.me>
2026-07-21 12:49:42 +0900

    Update ruca.sh

--- ruca.sh
+++ ruca.sh
@@ -1,4 +1,10 @@
-cat $1 | ruca > main.asm
-nasm -w-implicit-abs-deprecated -f elf64 -g -F dwarf -o main.o main.asm
-gcc main.o $(pkg-config --cflags --libs gtk+-3.0) -no-pie -z noexecstack -O3 -rdynamic -o main
-./main
+run() {
+    cat $1 | ruca > main.asm
+    nasm -w-implicit-abs-deprecated -f elf64 -g -F dwarf -o main.o main.asm
+    gcc main.o $(pkg-config --cflags --libs gtk+-3.0) -no-pie -z noexecstack -O3 -rdynamic -o main
+}
+
+for src in $@; do
+    run src
+    ./main
+done
