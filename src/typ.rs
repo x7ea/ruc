c766cc94b7c8a682d1f117d0992d8f2b2f254573
Flandre Scarlet <compiler-develop@proton.me>
2026-07-21 18:25:32 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -76,7 +76,9 @@ impl Expr {
             ($typ: pat, $lhs: expr, $rhs: expr) => {
                 match ($lhs.infer(ctx)?, $rhs.infer(ctx)?) {
                     ($typ, ret @ $typ) => typing!(ret.clone()),
-                    (lhs, rhs) if lhs != rhs => Err(format!("operator term: {lhs} != {rhs}")),
+                    (lhs, rhs) if lhs != rhs => {
+                        Err(format!("Operator::{} term: {lhs} != {rhs}", self.as_ref()))
+                    }
                     (typ, _) => typing!(expands!(Expr::Call(
                         Box::new(var!(&self.as_ref().to_lowercase(), &typ)),
                         vec![*$lhs, *$rhs],
