d2ccb867bf2bab9f10dc70a34b35fc7029eeee54
Flandre Scarlet <compiler-develop@proton.me>
2026-07-21 17:46:22 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -332,11 +332,11 @@ macro_rules! map {
 #[macro_export]
 macro_rules! var {
     ($name: expr) => {{ Expr::Variable(Generic(Name::new(&$name)?, Vec::new())) }};
-    ($name: expr, $typ: block) => {{
+    ($name: expr, $arg: block) => {{ Expr::Variable(Generic(Name::new(&$name)?, vec![$arg])) }};
+    ($name: expr, $typ: expr) => {{
         Expr::Variable(Generic(
             Name::new($name)?.class(&$typ.remove_generic()),
             $typ.generic_args(),
         ))
     }};
-    ($name: expr, $arg: expr) => {{ Expr::Variable(Generic(Name::new(&$name)?, vec![$arg])) }};
 }
