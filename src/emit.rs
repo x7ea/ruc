328fef87bbee5e04a3424eff39319e1550b31921
Flandre Scarlet <compiler-develop@proton.me>
2026-07-27 10:13:54 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -269,7 +269,10 @@ impl Expr {
                 "{}\tadd rdx, rsi\n\tmov rax, rdx\n\tcqo\n\tidiv rsi\n\tmov rax, rdx\n",
                 expr!(self).emit(ctx)?
             )),
-            Expr::Not(bool) => Ok(format!("{}\tnot rax\n", bool.emit(ctx)?)),
+            Expr::Not(bool) => Ok(format!(
+                "{}\tcmp rax, 0\n\tsete al\n\tmovzx rax, al\n",
+                bool.emit(ctx)?
+            )),
             Expr::Add(lhs, rhs) => Ok(op!("add", lhs, rhs)),
             Expr::Sub(lhs, rhs) => Ok(op!("sub", lhs, rhs)),
             Expr::Mul(lhs, rhs) => Ok(op!("mul", lhs, rhs)),
