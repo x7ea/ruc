da55e028f524b23555d6588fb63437ff5a46949a
Flandre Scarlet <compiler-develop@proton.me>
2026-07-27 23:43:09 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -151,7 +151,7 @@ pub struct Global {
 pub struct Function {
     var: IndexMap<Name, Type>,
     scope: IndexMap<Name, Type>,
-    raii: IndexSet<Name>,
+    raii: IndexSet<Expr>,
     typed: IndexMap<Expr, Type>,
     expand: IndexMap<Expr, Expr>,
     class: Option<Type>,
