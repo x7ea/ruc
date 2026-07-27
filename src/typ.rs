5183a54047a87830172669e1fc2d87cf3db4fd36
Flandre Scarlet <compiler-develop@proton.me>
2026-07-27 23:54:39 +0900

    Update typ.rs

--- src/typ.rs
+++ src/typ.rs
@@ -275,7 +275,7 @@ impl Expr {
                     Expr::Variable(Generic(name, _)) => {
                         let val = val.infer(ctx)?;
                         if let Some(typ) = ctx.local.scope.get(name) {
-                            let typ = typ.clone().solve(ctx);
+                            let typ = typ.solve(ctx);
                             if val != typ {
                                 return Err(format!("{name}: {typ} != {val}"));
                             }
