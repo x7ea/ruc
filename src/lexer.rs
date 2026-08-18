6a800f31109ef875abe08e3250cac10ab198b4a4
Flandre Scarlet <compiler-develop@proton.me>
2026-08-18 16:40:45 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -161,13 +161,5 @@ macro_rules! once {
         } else {
             Err(format!("expected: {}", $del))
         }
-    }};
-    ($v: expr, $del: literal, right) => {{
-        let v = lexer($v, $del)?;
-        if v.len() >= 2 {
-            Ok((v[..v.len() - 1].join($del), v[v.len() - 1].clone()))
-        } else {
-            Err(format!("expected: {}", $del))
-        }
-    }};
+
 }
