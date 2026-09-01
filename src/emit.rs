389a742ad53701d117c4e23ec03e12d2b669a5b5
Flandre Scarlet <compiler-develop@proton.me>
2026-09-01 16:34:38 +0900

    Update emit.rs

--- src/emit.rs
+++ src/emit.rs
@@ -15,6 +15,7 @@ impl Define {
         let ctx = &mut Context::default();
         ctx.global.def = defines.iter().map(|x| (name!(x), x.clone())).collect();
         map!({ defines }, |define| define.infer(ctx))?;
+        ctx.global.used.insert(Name::new("main")?);
 
         let mut text = String::from("\n");
         for (_, define) in ctx.global.def.clone() {
