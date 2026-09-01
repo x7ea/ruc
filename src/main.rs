fa7c919692e6b61f5a69b16195506c2ddee2d607
Flandre Scarlet <compiler-develop@proton.me>
2026-09-01 16:29:18 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -142,7 +142,7 @@ pub struct Global {
     table: IndexMap<Name, (Vec<Type>, Object)>,
     alias: IndexMap<Type, Type>,
     extrn: IndexSet<Name>,
-    used: IndexSet<Name>,
+    used: IndexMap<Name, usize>,
 }
 
 #[derive(Debug, Default, Clone)]
