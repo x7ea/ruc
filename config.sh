5627bdac01b7555e3d153885151423e4a6b9a33a
Flandre Scarlet <compiler-develop@proton.me>
2026-09-02 14:06:51 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
-apt install git curl build-essential
+apt install git curl build-essential libgtk-3-0 libgtk-3-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 git clone https://github.com/x7ea/ruc.git
 cd ./ruc
 
 cargo install --path .
-./ruc.sh  
+./ruc.sh ./app/hello.rc
