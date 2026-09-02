3b7ca34a5d276549dae45b1ebe250a5734bff85a
Flandre Scarlet <compiler-develop@proton.me>
2026-09-02 14:17:21 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-apt install git curl build-essential libgtk-3-0 libgtk-3-dev
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-git clone https://github.com/x7ea/ruc.git
-cd ./ruc
-
-cargo install --path .
-./ruc.sh ./app/hello.rc
